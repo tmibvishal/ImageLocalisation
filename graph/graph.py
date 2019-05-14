784e580e35c624dfde0fa571f54e103f5b3a4d35
vishalBindal <vishalbindal.06@gmail.com>
2019-05-14 15:09:23 +0530

    add mouse click functionalitites

--- graph/graph.py
+++ graph/graph.py
@@ -1,63 +1,167 @@
 import numpy as np
 import cv2
+import math
+
+
 class Node:
-        def __init__(self, id, name, x__y__z, links):
+        def __init__(self, id, name, x, y, z, links):
             self.id = id
             self.name = name
-            self.coordinates = (x,y,z)
+            self.coordinates = (x, y, z)
             self.links = links
 
-class Graph:
 
-    Nodes = {}
+class Graph:
+    Nodes = []
     Length = 0
 
     def __init__(self):
         return
 
-    def create_node(self, name, x__y__z, links):
+    def create_node(self, name, x, y, z, links):
         id = self.Length
-        Nd = Node(id, name, (x,y,z), links)
+        Nd = Node(id, name, x, y, z, links)
         self.add_node(Nd)
         
     def add_node(self, Nd):
-        if(isinstance(Nd, Node) and Nd.name not in self.Nodes):
+        if isinstance(Nd, Node) and Nd.name not in self.Nodes:
             # Check if Nd has the format of Node or not
-            if(isinstance(Nd.links, list)):
+            if isinstance(Nd.links, list):
                 # Check if Nd.links is a list
-                if(len(Nd.links) != 0):
-                    if(isinstance(Nd.links[0], int)):
+                if len(Nd.links) != 0:
+                    if isinstance(Nd.links[0], int):
                         # Check if Nd.list is a list of integers
-                        self.Nodes[Nd.id] = Nd
-                        self.Length += 1
+                        self.Nodes.append(Nd)
+                        self.Length = self.Length + 1
                     else:
                         raise Exception("Nd.list is not a list of ids (which are integers)")
                 else:
-                    self.Nodes[Nd.id] = Nd
-                    self.Length += 1
+                    self.Nodes.append(Nd)
+                    self.Length = self.Length + 1
             else:
                 raise Exception("Nd.list is not a list")
         else:
-            raise Exception("Nd format is not of Node")
+            raise Exception("Nd format is not of Node, or is already present")
 
     def get_node(self, id):
-        if(id<self.Length):
-            return Nodes[id]
+        if id < self.Length:
+            return self.Nodes[id]
         else:
-            raise Exception("id does not exists")
+            raise Exception("id does not exist")
+
+    def nearest_node(self,x,y):
+        def distance(nd):
+            delx = abs(nd.coordinates[0]-x)
+            dely = abs(nd.coordinates[1]-y)
+            return math.sqrt(delx**2 + dely**2)
+
+        min, nearest_node = -1, None
+        for nd in self.Nodes:
+            if abs(nd.coordinates[0]-x)<50 and abs(nd.coordinates[1]-y)<50:
+                if min == -1 or distance(nd)<min:
+                    nearest_node = nd
+                    min = distance(nd)
+                elif distance(nd)==min:
+                    print("Equal distance")
+                    return None
+        print("Node found")
+        return nearest_node
+
+    def connect(self,nd1,nd2):
+        if isinstance(nd1,Node) and isinstance(nd2,Node):
+            if nd2.id not in nd1.links:
+                nd1.links.append(nd2.id)
+            if nd1.id not in nd2.links:
+                nd2.links.append(nd1.id)
+
+    def printnode(self,nd):
+        print(nd.coordinates[0])
+        print(nd.coordinates[1])
+
 
     def print_graph(self):
-        img = np.ones([512,512,3], np.uint8)
+        img = np.ones([512, 512, 3], np.uint8)
         for Nd in self.Nodes:
-            img = cv2.circle(img, (Nd.coordinates[0],Nd.coordinates[1]), 2, (0,0,0), -1)
+            img = cv2.circle(img, (Nd.coordinates[0], Nd.coordinates[1]), 8, (66, 126, 255), -1)
             for linkId in Nd.links:
-                if(linkId<self.Length):
-                    img = cv2.arrowedLine(img, (Nd.coordinates[0],Nd.coordinates[1]) , (Nodes[linkId].coordinates[0], Nodes[linkId].coordinates[1]) , (0,0,0), 3, cv2.LINE_AA)
+                if linkId < self.Length:
+                    Nd2 = self.Nodes[linkId]
+                    img = cv2.line(img, (Nd.coordinates[0], Nd.coordinates[1]), (Nd2.coordinates[0], Nd2.coordinates[1]), (66, 126, 255), 1, cv2.LINE_AA)
                 else:
                     raise Exception("linkId does not exists")
-        cv2.imshow('image', img)
+        cv2.imshow('Node graph', img)
         cv2.waitKey(0)
+        cv2.imwrite('nodegraph.jpg', img)
+        cv2.destroyAllWindows()
+
+    def mark_nodes(self):
+        def click_event(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                id = self.Length
+                self.create_node('Node-'+str(id), x, y, 0, [])
+                cv2.circle(img, (x, y), 8, (66, 126, 255), -1)
+                cv2.imshow('Mark Nodes', img)
+
+        img = cv2.imread('nodegraph.jpg')
+        if img.size == 0:
+            img = np.ones([512, 512, 3], np.uint8)
+        cv2.imshow('Mark Nodes', img)
+
+        cv2.setMouseCallback('Mark Nodes', click_event)
+
+        cv2.waitKey(0)
+        cv2.imwrite('nodegraph.jpg', img)
+        cv2.destroyAllWindows()
+
+    def make_connections(self):
+        def click_first(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                print("Looking for node")
+                nd = self.nearest_node(x, y)
+                self.printnode(nd)
+                if nd is not None:
+                    return nd
+
+        def click_second(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                nd = self.nearest_node(x, y)
+                if nd is not None:
+                    nd2 = param[0]
+                    self.connect(nd, nd2)
+                    cv2.line(img, (nd.coordinates[0], nd.coordinates[1]),
+                                   (nd2.coordinates[0], nd2.coordinates[1]), (66, 126, 255), 1, cv2.LINE_AA)
+                    cv2.imshow('Make connections', img)
+                    return True
+                else:
+                    return False
+
+        img = cv2.imread('nodegraph.jpg')
+        if img.size == 0:
+            img = np.ones([512, 512, 3], np.uint8)
+        cv2.imshow('Make connections', img)
+
+        pairs_complete = True
+        last_node = None
+
+        while cv2.waitKey(0) is None:
+            if pairs_complete is True:
+                last_node = cv2.setMouseCallback('Make connections', click_first)
+                if last_node is not None:
+                    pairs_complete = False
+
+            else:
+                pairs_complete = cv2.setMouseCallback('Make connections', click_second, last_node)
+
+        cv2.imwrite('nodegraph.jpg', img)
         cv2.destroyAllWindows()
 
 
-                
+graph = Graph()
+graph.create_node('entrance', 256, 256, 0, [1])
+graph.create_node('lift-grd', 100, 500, 0, [0])
+graph.print_graph()
+graph.mark_nodes()
+graph.make_connections()
+graph.print_graph()
+#events = [i for i in dir(cv2) if 'EVENT' in i]
+#print(events)
