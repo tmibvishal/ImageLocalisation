72c1a78135f376ae40573b77d31d9b08fb56180d
vishalBindal <vishalbindal.06@gmail.com>
2019-05-18 00:00:04 +0530

    Compare 2 videos for preliminary testing

--- videoOperations.py
+++ videoOperations.py
@@ -7,39 +7,73 @@ import cv2
 import numpy as np
 import matcher as mt
 import os
+import time
+
+
 # path = './Images'
 
-cap = cv2.VideoCapture("VID_20190516_235550.mp4")
-# cap= cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,50)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,50)
 
-distinct_frames=[]
-comparison_frame =None
-i=0
-a= None
-b= None
+def save_frames(video_str, folder):
+    if video_str == "webcam":
+        video_str = 0
+    cap = cv2.VideoCapture(video_str)
+    # cap= cv2.VideoCapture(0)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 200)
 
+    distinct_frames = []
+    comparison_frame = None
+    i = 0
+    a = None
+    b = None
 
-ret, frame = cap.read()
-a = frame
-cv2.imwrite('image' + str(i) + '.jpg', b)
-
-while(True):
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    
-    b = frame
-    image_fraction_matched = mt.ORB_match(a,b,2500,0.7)       
-    if (image_fraction_matched < 0.1):
-        cv2.imwrite('image' + str(i) + '.jpg', a)
-        a = b
+    a = frame
+    cv2.imwrite('image' + str(i) + '.jpg', b)
 
-    i = i + 1
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        b = frame
+        image_fraction_matched = mt.SURF_match(a, b, 2500, 0.7)
+        if image_fraction_matched < 0.1:
+            cv2.imwrite(folder + '/image' + str(i) + '.jpg', a)
+            distinct_frames.append((i, a))
+            a = b
 
-    if cv2.waitKey(1) & 0xFF== ord('q'):
-        break
+        i = i + 1
 
-print(i)
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return distinct_frames
+
+
+def compare_videos():
+    frames1 = save_frames("testData/sushant_mc/20190517_220001.mp4", "v1")
+    frames2 = save_frames("testData/sushant_mc/20190517_220439.mp4", "v2")
+
+    len1, len2 = len(frames1), len(frames2)
+    best_matches = []
+    for i in range(len1):
+        print("")
+        print(str(frames1[i][0])+":")
+        best_matches_for_i = []
+        for j in range(len2):
+            image_fraction_matched = mt.SURF_match(frames1[i][1], frames2[j][1], 2500, 0.7)
+            if image_fraction_matched > 0.2:
+                print(str(frames2[j][0])+" : confidence is "+str(image_fraction_matched))
+                best_matches_for_i.append((frames2[j][0], image_fraction_matched))
+        best_matches.append((frames1[i][0], best_matches_for_i))
+
+
+compare_videos()
+
+
+
+
+
+
+
