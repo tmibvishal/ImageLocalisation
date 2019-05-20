b71d403a033bfcd56da8be44ab809551262d76b7
Vishal Singh <tmibvishal@gmail.com>
2019-05-20 19:26:03 +0530

    fix bugs

--- video_operations.py
+++ video_operations.py
@@ -44,7 +44,7 @@ def is_blurry(image):
     return (variance_of_laplacian(b) < 120)
 
 
-def save_distinct_frames(video_str, folder, frames_skipped=0):
+def save_distinct_frames(video_str, folder, frames_skipped:int=0,check_blurry:bool=False):
     """Saves non redundent and distinct frames of a video in folder
     Parameters
     ----------
@@ -183,7 +183,7 @@ def edge_from_specific_pt(i_init, j_init, frames1, frames2):
     """
     i = index of current frame (in frames1) being checked for matches; i_last_matched<i<len(frames1)
     i_last_matched = index of last frame (in frames1 ) matched; i_init<=i_last_matched<len(frames1)
-    i_last_matched = index of last frame (in frames2 ) matched(with i_last_matched);
+    j_last_matched = index of last frame (in frames2 ) matched(with i_last_matched);
                         j_init<=j_last_matched<len(frames2)
     match = index of best matched frame (in frames2) with (i)th frame in frames1. j_last_matched<=match<=j
     maxmatch = fraction matching between (i)th and (match) frames
