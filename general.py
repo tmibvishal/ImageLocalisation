922a162802fcc52d339d3e7fd98fd7601cad73a6
vishalBindal <vishalbindal.06@gmail.com>
2019-05-27 21:25:45 +0530

    Some changes to graph.py

--- general.py
+++ general.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 def ensure_path(path: str):
     """Takes the path as a string and ensures the path exists. 
@@ -26,3 +27,42 @@ def ensure_path(path: str):
             except:
                 return False
     return True
+
+
+def load_from_memory(file_name: str, folder: str = None):
+    """
+    Load a python object saved as .pkl from the memory
+
+    :param file_name: name of the file
+    :param folder: name of the folder, folder = None means current folder
+    :return: pyobject or False if fails to load
+    """
+    try:
+        with open(folder + "/" + file_name, 'rb') if folder is not None else open(file_name, 'rb') as input_rb:
+            pyobject = pickle.load(input_rb)
+            return pyobject
+    except Exception as exception:
+        return False, exception
+
+
+def save_to_memory(pyobject, file_name: str, folder: str = "."):
+    """
+    Save a pyobject to the memory
+
+    :param pyobject: python object
+    :param file_name: file_name that pyobject will be saved with
+    :param folder: name of the folder where to save, folder = None means current folder
+    :return: True if file is loader or False otherwise
+    """
+
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+
+    try:
+        with open(folder + "/" + file_name, 'wb') if folder is not None else open(file_name, 'wb') as output:
+            pickle.dump(pyobject, output, pickle.HIGHEST_PROTOCOL)
+        return True
+    except Exception as e:
+        raise e
