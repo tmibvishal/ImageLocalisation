1aa8d351790604d4391ca2f67b118d5af64905ab
sushantsondhi <41734795+sushantsondhi@users.noreply.github.com>
2019-05-28 23:25:49 +0530

    match node function

--- node and edge_localisation.py
+++ node and edge_localisation.py
@@ -30,37 +30,34 @@ class node_and_image_maching:
             node_images= node.node_images
             for j in range(no_of_frames_of_query_video_to_be_matched):
                 for k in range(len(node_images)):
-                    image_fraction_matched = mt.SURF_match_2((query_video_frames[j][1], query_video_frames[j][2]), (node_images[k][1], node_images[k][2]),
+                    image_fraction_matched = mt.SURF_match_2(query_video_frames.get_object(j).get_elements(), node_images.get_object(k).get_elements(),
                                                          2500, 0.7)
                     if image_fraction_matched> 0.15:
                        confidence= confidence+1
-            if (confidence/no_of_frames_of_query_video_to_be_matched >0.32)
-                   matched_nodes.append(i)
-fdsa
+            if confidence/no_of_frames_of_query_video_to_be_matched >0.32:
+                self.matched_nodes.append(node)
+
 
     # def locate_edge(self,query_video_frames, confidence_level:int=4):
-    #     global matched_nodes
-    #     global matched_edges
-    #     for i in matched_nodes:
-    #         for j in #linkd edges of matched nodes:
-    #             matched_edges.append((#edge, 0, 0)) #(edge, confidence, frame_position_matched)
+    #     for node in self.matched_nodes:
+    #         for edge in node.links:
+    #             self.matched_edges.append((edge, 0, 0)) #(edge, confidence, frame_position_matched)
     #
     #     for i in query_video_frames:
-    #         for j in matched_edges:
-    #             match, maxmatch= None, 0
-    #             for k in #starting from matched_edges[j][2] till end #edge folder:
-    #                 # image_fraction_matched = mt.SURF_match_2((frames1[i][1], frames1[i][2]), (frames2[j][1], frames2[j][2]),
-    #                 2500, 0.7)
+    #         for j in self.matched_edges:
+    #             match, maximum_match= None, 0
+    #             for k in range(int(j[2]),len(j[0].distinct_frames)):#starting from matched_edges[j][2] till end #edge folder
+    #                 image_fraction_matched = mt.SURF_match_2((j[0].distinct_frames[k][1], j[0].distinct_frames[k][2]), (i[1], i[2]), 2500, 0.7))
     #                 if image_fraction_matched >0.15:
-    #                     if image_fraction_matched > maxmatch:
-    #                         match, maxmatch = j, image_fraction_matched
+    #                     if image_fraction_matched > maximum_matchmatch:
+    #                         match, maxmatch = k, image_fraction_matched
     #             if match is not None:
-    #                 matched_edges[j]=(matched_edges[j][0],matched_edges[j][1]+1, match)
+    #                 self.matched_edges[j]=(self.matched_edges[j][0],self.matched_edges[j][1]+1, match)
     #             else:
-    #                 matched_edges[j]=(matched_edges[j][0],matched_edges[j][1]-1, matched_edges[j][2])
-    #             if matched_edges[j][1]< (-1)*confidence_level:
-    #                 matched_edges.pop(j)
-    #             if matched_edges[j][1]> confidence_level and len(matched_edges)==1:
+    #                 self.matched_edges[j]=(self.matched_edges[j][0],self.matched_edges[j][1]-1, self.matched_edges[j][2])
+    #             if self.matched_edges[j][1]< (-1)*confidence_level:
+    #                 self.matched_edges.pop(j)
+    #             if self.matched_edges[j][1]> confidence_level and len(self.matched_edges)==1:
     #                 print("edge found")
     #                 break
 
