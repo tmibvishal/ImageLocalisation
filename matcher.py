d7d8fd9e100cd69834251935e3a0e27a57dcfc39
vishalBindal <vishalbindal.06@gmail.com>
2019-06-18 18:39:32 +0530

    Add SURF_returns matcher

--- matcher.py
+++ matcher.py
@@ -16,6 +16,7 @@ def cos_cdist(self, des1, des2):
     v = des1.reshape(1, -1)
     return scipy.spatial.distance.cdist(np.array([des2]), v, 'cosine').reshape(-1)
 
+
 def KAZE_match(des1, des2):
     # reference: https://medium.com/machine-learning-world/feature-extraction-and-similar-image-search-with-opencv-for-newbies-3c59796bf774
     img_distances = cos_cdist(des1, des2)
@@ -24,7 +25,8 @@ def KAZE_match(des1, des2):
     return 1 - not_match
 
 
-def SURF_match_2(key_des_1,key_des_2, hessianThreshold: int = 400, ratio_thresh: float = 0.7, symmetry_match: bool = True):
+def SURF_match_2(key_des_1, key_des_2, hessianThreshold: int = 400, ratio_thresh: float = 0.7,
+                 symmetry_match: bool = True):
     """Give fraction match between 2 images descriptors using SURF and FLANN
 
     Parameters
@@ -65,7 +67,7 @@ def SURF_match_2(key_des_1,key_des_2, hessianThreshold: int = 400, ratio_thresh:
             good_matches.append(m)
     c1 = len(good_matches)
 
-    if(symmetry_match):
+    if (symmetry_match):
         knn_matches = matcher.knnMatch(descriptors2, descriptors1, 2)
         good_matches = []
         for m, n in knn_matches:
@@ -76,9 +78,10 @@ def SURF_match_2(key_des_1,key_des_2, hessianThreshold: int = 400, ratio_thresh:
         return fraction
 
     fraction = (2.0 * c1) / (a1 + b1)
-    if(fraction > 1): fraction = 1
+    if (fraction > 1): fraction = 1
     # fraction can be greater than one in blur images because we are multiplying fraction with 2
     return fraction
+
 
 def SURF_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.7, symmetry_match: bool = True):
     """Give fraction match between 2 images using SURF and FLANN
@@ -89,6 +92,7 @@ def SURF_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.
     img2 : Open CV image format,
     hessianThreshold: Number of ORB points to consider in a image,
     ratio_thresh: (b/w 0 to 1) lower the number more serious the matching
+    symmetry_match:
 
     Returns
     -------
@@ -97,11 +101,9 @@ def SURF_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.
     """
     if img1 is None or img2 is None:
         raise Exception("img1 or img2 can't be none")
-        return -1
 
     if ratio_thresh > 1 or ratio_thresh < 0:
         raise Exception("ratio_thresh not between 0 to 1")
-        return -1
 
     detector = cv2.xfeatures2d_SURF.create(hessianThreshold)
     keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
@@ -121,7 +123,7 @@ def SURF_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.
             good_matches.append(m)
     c1 = len(good_matches)
 
-    if(symmetry_match):
+    if symmetry_match:
         knn_matches = matcher.knnMatch(descriptors2, descriptors1, 2)
         good_matches = []
         for m, n in knn_matches:
@@ -132,7 +134,8 @@ def SURF_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.
         return fraction
 
     fraction = (2.0 * c1) / (a1 + b1)
-    if(fraction > 1): fraction = 1
+    if fraction > 1:
+        fraction = 1
     # fraction can be greater than one in blur images because we are multiplying fraction with 2
     return fraction
 
@@ -179,3 +182,130 @@ def ORB_match(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.7
 
     return (2.0 * c1) / (a1 + b1)
 
+
+def SURF_returns(img1, img2, hessianThreshold: int = 400, ratio_thresh: float = 0.7, symmetry_match: bool = True,
+                 max_slope=0.2, check_c1_c2:bool = True):
+    """Give fraction match between 2 images using SURF and FLANN
+
+    Parameters
+    ----------
+    img1 : Open CV image format,
+    img2 : Open CV image format,
+    hessianThreshold: Number of ORB points to consider in a image,
+    ratio_thresh: (b/w 0 to 1) lower the number more serious the matching
+    symmetry_match
+    max_slope : Ensures |slope| of line connecting matching pts is less than max_slope
+    check_c1_c2 : Ensures c1, c2 are less than 50% deviated from each other in symmetry_match
+
+    Returns
+    -------
+    float,
+        returns a number from 0 to 1 depending on percentage match and returns -1 if any of the parameter is None
+    """
+    if img1 is None or img2 is None:
+        raise Exception("img1 or img2 can't be none")
+
+    if ratio_thresh > 1 or ratio_thresh < 0:
+        raise Exception("ratio_thresh not between 0 to 1")
+
+    detector = cv2.xfeatures2d_SURF.create(hessianThreshold)
+    keypoints1, descriptors1 = detector.detectAndCompute(img1, None)
+    keypoints2, descriptors2 = detector.detectAndCompute(img2, None)
+
+    import video_operations_3 as vo3
+    kp1 = vo3.serialize_keypoints(keypoints1)
+    kp2 = vo3.serialize_keypoints(keypoints2)
+
+    a1 = len(kp1)
+    b1 = len(kp2)
+
+    if a1 < 2 or b1 < 2:
+        return 0
+
+    matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+    knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
+    good_matches = []
+
+    for m, n in knn_matches:
+        if m.distance < ratio_thresh * n.distance:
+
+            # Calculation of slope
+            img2_idx = m.trainIdx
+            img1_idx = m.queryIdx
+            (x1, y1) = kp1[img1_idx][0]
+            (x2_rel, y2_rel) = kp2[img2_idx][0]
+            (x2, y2) = (x2_rel + img1.shape[0], y2_rel)
+            if x2 < x1:
+                raise Exception("x1 somehow greater than x2")
+            elif x2 == x1:
+                continue
+            else:
+                slope = (y1 - y2) / (x2 - x1)  # Since y is measured from upper edge of frame
+
+            if abs(slope) > max_slope:
+                continue
+            # Appending to good_matches
+            good_matches.append(m)
+
+            # Testing ( one match )
+
+            # print(slope)
+            # img3 = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+            # cv2.drawMatches(
+            #     img1, keypoints1, img2, keypoints2, [m], outImg=img3, matchColor=None, flags=2)
+            # if img3 is not None:
+            #     cv2.imshow("matches", img3)
+            #     cv2.waitKey(0)
+            #     cv2.destroyAllWindows()
+
+    c1 = len(good_matches)
+
+    # Testing ( all matches )
+    # img3 = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3), dtype=np.uint8)
+    # cv2.drawMatches(
+    #     img1, keypoints1, img2, keypoints2, good_matches, outImg=img3, matchColor=None, flags=2)
+    # if img3 is not None:
+    #     cv2.imshow("matches", img3)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+
+    if symmetry_match:
+        knn_matches = matcher.knnMatch(descriptors2, descriptors1, 2)
+        good_matches = []
+        for m, n in knn_matches:
+            if m.distance < ratio_thresh * n.distance:
+                # Calculation of slope
+                img2_idx = m.trainIdx
+                img1_idx = m.queryIdx
+                (x1, y1) = kp2[img1_idx][0]
+                (x2_rel, y2_rel) = kp1[img2_idx][0]
+                (x2, y2) = (x2_rel + img2.shape[0], y2_rel)
+                if x2 < x1:
+                    raise Exception("x1 somehow greater than x2")
+                elif x2 == x1:
+                    continue
+                else:
+                    slope = (y1 - y2) / (x2 - x1)  # Since y is measured from upper edge of frame
+
+                if abs(slope) > max_slope:
+                    continue
+                # Appending to good_matches
+                good_matches.append(m)
+
+        c2 = len(good_matches)
+
+        if check_c1_c2:
+            if c1/c2 not in (0.5, 1.5):
+                return 0
+        fraction = (c1 + c2) / (a1 + b1)
+        return fraction
+
+    if c1 > b1:
+        return 0
+    fraction = (2.0 * c1) / (a1 + b1)
+    return fraction
+
+
+img1 = cv2.imread("edge_data/edge_0_1/jpg/image0.jpg")
+img2 = cv2.imread("query_distinct_frame/case1/jpg/image0.jpg")
+a = SURF_returns(img1, img2)
